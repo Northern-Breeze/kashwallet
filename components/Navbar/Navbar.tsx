80499c1dd468afa71d1333ee2faf241cdb6d06cf
thagana <samuelmthwa79@gmail.com>
2022-11-10 23:20:24 +0200

    top part done

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
         <nav className="navbar py-4">
             <div className="container is-fluid">
                 <div className="navbar-brand">
-                    <a className="navbar-item" href="#">
+                    <a className="navbar-item" href="/">
                         <img className="image" src="/images/logo.png" alt="2pay logo" height="100px"/>
                     </a>
                     <a className="navbar-burger" role="button" aria-label="menu" aria-expanded="false">
@@ -15,8 +15,8 @@ export default function Navbar() {
                     </a>
                 </div>
                 <div className="navbar-menu">
-                    <div className="navbar-start"><a className="navbar-item" href="#">About</a><a className="navbar-item"
-                        href="#">Company</a><a className="navbar-item" href="#">Services</a></div>
+                    <div className="navbar-start"><a className="navbar-item" href="/about">About</a><a className="navbar-item"
+                        href="/contact-us">Contact Us</a><a className="navbar-item" href="/services">Services</a></div>
                     <div className="navbar-item">
                         <div className="field has-addons">
                             <div className="control">
@@ -24,7 +24,7 @@ export default function Navbar() {
                             </div>
                             <div className="control">
                                 <button className="button" type="submit">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewbox="0 0 24 24" stroke="currentColor"
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
                                         style={{ width: '24px', height: '24px' }}>
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                             d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
---
+++ pages/about.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react'
+import { motion, AnimatePresence } from "framer-motion";
+
+export default function about() {
+  return (
+    <div className='about_wrapper'>
+      <div className='left-info'>
+        <div>
+          <AnimatePresence>
+            <motion.h1
+              className='mb-4 is-size-1 is-size-3-mobile has-text-weight-bold color-header'
+              initial={{ x: -300, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              exit={{ x: -300, opacity: 0 }}
+            >
+              About 2pay
+            </motion.h1>
+          </AnimatePresence>
+        </div>
+        <AnimatePresence>
+          <motion.p className='description'
+            initial={{ x: -300, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            exit={{ x: -300, opacity: 0 }}
+          >
+            2pay is a payment platform that allow individuals and business to make and receive payments easily
+          </motion.p>
+        </AnimatePresence>
+      </div>
+      <div className='right-image'>
+        <AnimatePresence>
+          <motion.img
+            src='/images/Phone_Screenshot_.jpg'
+            className='mx-auto mt-0-widescreen is-block show-image-1'
+            initial={{ x: 300, opacity: 0 }}
+            animate={{ x: 0, opacity: 1, rotate: -5 }}
+            exit={{ x: -300, opacity: 0 }}
+          />
+        </AnimatePresence>
+        <AnimatePresence>
+          <motion.img
+            src='/images/Phone_Screenshot__.jpg'
+            className='mx-auto mt-0-widescreen is-block show-image'
+            initial={{ x: 300, opacity: 0 }}
+            animate={{ x: 0, opacity: 1, rotate: 5 }}
+            exit={{ x: -300, opacity: 0 }}
+          />
+        </AnimatePresence>
+      </div>
+    </div>
+  )
+}
